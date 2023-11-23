556a1a24e23c66daa57774f9d182e78476145d35
jaehoshin123 <76482912+jaehoshin123@users.noreply.github.com>
2023-11-23 18:07:36 +0900

    Delete UMC_NODE_WORKBOOK-week_8 directory

--- UMC_NODE_WORKBOOK-week_8/src/routes/temp.route.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { tempTest, tempException } from '../controllers/temp.controller.js';
-
-export const tempRouter = express.Router();
-
-tempRouter.get('/test', tempTest);
-
-tempRouter.get('/exception/:flag',tempException);
